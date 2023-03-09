6d6c50acd19bde5287e94aa322131e637d232be9
Guilherme Medeiros da Cunha <gmcunha@inf.ufsm.br>
2023-03-09 15:19:55 -0300

    Implementa funcionalidades de página inicial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,20 @@ class HomeController < ApplicationController
     @lists = List.all
     @items = Item.all
   end
+  
+  def clear_lists
+    List.left_outer_joins(:items).where(items: { id: nil }).destroy_all
+    respond_to do |format|
+      format.html { redirect_to root_url }
+      format.json { head :no_content }
+    end
+  end
+  
+  def clear_items
+    Item.where(marked: true).destroy_all
+    respond_to do |format|
+      format.html { redirect_to root_url }
+      format.json { head :no_content }
+    end
+  end
 end
